22086f2052ef36ab6b3f21397a8dd70eff0798a3
tpppeggi <tpppeggi@gmail.com>
2018-01-16 14:24:06 +0800

    Niner

---
+++ exam/src/exam/Niner.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class Niner {
+	public static void main(String[] args){
+		for(int i = 1;i<10;i++){
+			for(int j = 1;j<6;j++){
+				System.out.print(j+"x"+i+"=" +j*i+"\t");
+			}
+		System.out.println();
+		}
+		
+	}
+
+}
