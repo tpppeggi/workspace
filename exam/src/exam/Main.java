f90a8a2503cb6c6d00d0a5f1b31dd641ad299376
tpppeggi <tpppeggi@gmail.com>
2018-01-16 15:06:16 +0800

    Ninar2

---
+++ exam/src/exam/Main.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i, j;
+	}
+	
+class ArrayList{
+	
+	
+	
+}
+}
---
+++ exam/src/exam/VendingMachine.java
@@ -0,0 +1,27 @@
+package exam;
+
+public class VendingMachine {
+
+	public static void main(String[] args) {
+		int i, j;
+		CA customer = new CA(70);
+
+	}
+
+	class CA {
+		public void func(int X) {
+			int a = 10;
+			int b = 15;
+			int c = 10;
+			int d = 18;
+			int e = 10;
+			int f = 20;
+			int g = 15;
+			int h = 18;
+			if (X > 1) {
+				System.out.print("投入金額 " + X);
+			}
+		}
+	}
+
+}
